e469e5a1a44b49f40074de9e8000f252d2632b30
Briar Zimmerman <briarzimmerman@Briars-MBP.attlocal.net>
2022-06-16 14:48:31 -0700

    chore: small change to check deploy pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This page is under development! Stay tuned...
+          This page is under development! We are working hard to bring you quality content. Stay tuned...
         </p>
       </header>
     </div>
